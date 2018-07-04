460af912901e7995884aaa081d91509d1efadff2
Amira Bennani <amirabennani1955m@gmail.com>
2018-07-04 23:00:08 +0100

    Removed spec comments.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,18 +1,5 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+
     describe('RSS Feeds', function() {
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
@@ -44,14 +31,7 @@ $(function() {
           })
     })
 
-    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries',function () {
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
         beforeEach(function(done){
           loadFeed(0,function () {
           });
@@ -63,7 +43,6 @@ $(function() {
         })
     })
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection',function () {
         let entries = $(".feed")[0].outerHTML;
         beforeEach(function(done){
@@ -71,12 +50,7 @@ $(function() {
           done() 
           });
         });
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
          it('should actually change the content',function (done) {
-          
           expect($(".feed")[0].outerHTML).not.toEqual(entries);
           done()
          })
